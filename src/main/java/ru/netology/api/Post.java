7ecc95c3a3ecb76bd8aade654bc0a3ec7b3c4633
TatyanaMilyutkina <milytkinata@gmail.com>
2022-06-09 16:06:18 +0400

    Inicial Commit

---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,16 @@
+package ru.netology.api;
+
+import javax.xml.crypto.Data;
+
+public class Post {
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+
+
+}
